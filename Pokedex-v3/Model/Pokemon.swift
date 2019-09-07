fb3a51f6442f33cb253e6cd5609f94a8bad3fc5f
AustinPotts <pottsevolvements@gmail.com>
2019-09-07 14:45:57 -0400

    Set Up Model Object

---
+++ Pokedex-v3/Model/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  Pokedex-v3
+//
+//  Created by Austin Potts on 9/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Pokemon: Decodable {
+    let name: String
+}
